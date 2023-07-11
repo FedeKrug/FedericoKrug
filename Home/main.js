dfbec8a6b65d082ed3a206b5aabd4a97a5d0201b
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-07-11 19:02:02 -0300

    Favicon for Stray Journey page

---
+++ Home/main.js
@@ -0,0 +1,7 @@
+console.log('Hola mundo!!');
+// let word = 'El programador pragmatico';
+// let color = 'amarillo';
+// let number = 782;
+// let decimal = 1.34342;
+// console.log(word + ' tiene un escritorio de color ' + color + ' y su numero favorito es ' + number + ' aunque no puede dejar de pensar en el ' + decimal);
+//alert("I'm an alert, nice to meet you and Hello World!");
